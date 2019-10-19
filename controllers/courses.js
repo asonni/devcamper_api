15ffd2a653b3db306d64f10e7b48138d877fb605
Aladdin Sonni <aladdin.sonni@gmail.com>
2019-10-19 02:18:42 +0200

    Section 6: Mongoose Advanced Querying & Relationships

---
+++ controllers/courses.js
@@ -0,0 +1,110 @@
+const Course = require('../models/Course');
+const Bootcamp = require('../models/Bootcamp');
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+
+// @desc      Get all courses
+// @route     GET /api/v1/courses
+// @route     GET /api/v1/bootcamps/:bootcampId/courses
+// @access    Public
+exports.getCourses = asyncHandler(async (req, res, next) => {
+  if (req.params.bootcampId) {
+    const courses = await Course.find({ bootcamp: req.params.bootcampId });
+    return res.status(200).json({
+      success: true,
+      count: courses.length,
+      data: courses
+    });
+  } else {
+    res.status(200).json(res.advancedResults);
+  }
+});
+
+// @desc      Get single courses
+// @route     GET /api/v1/courses/:id
+// @access    Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: 'bootcamp',
+    select: 'name description'
+  });
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: course
+  });
+});
+
+// @desc      Create courses
+// @route     POST /api/v1/bootcamps/:bootcampId/courses
+// @access    Private
+exports.createCourse = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(
+        `No bootcamp with the id of ${req.params.bootcampId}`,
+        404
+      )
+    );
+  }
+
+  const createdCourse = await Course.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    data: createdCourse
+  });
+});
+
+// @desc      Update courses
+// @route     PUT /api/v1/courses/:id
+// @access    Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let updatedCourse = await Course.findById(req.params.id);
+
+  if (!updatedCourse) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  updatedCourse = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: updatedCourse
+  });
+});
+
+// @desc      Delete courses
+// @route     DELETE /api/v1/courses/:id
+// @access    Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const deletedCourse = await Course.findById(req.params.id);
+
+  if (!deletedCourse) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  await deletedCourse.remove();
+
+  res.status(200).json({
+    success: true,
+    data: deletedCourse.id
+  });
+});
---
+++ routes/courses.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+
+const {
+  getCourses,
+  getCourse,
+  createCourse,
+  updateCourse,
+  deleteCourse
+} = require('../controllers/courses');
+
+const Course = require('../models/Course');
+const advancedResults = require('../middleware/advancedResults');
+
+router
+  .route('/')
+  .get(
+    advancedResults(Course, {
+      path: 'bootcamp',
+      select: 'name description'
+    }),
+    getCourses
+  )
+  .post(createCourse);
+
+router
+  .route('/:id')
+  .get(getCourse)
+  .put(updateCourse)
+  .delete(deleteCourse);
+
+module.exports = router;
