db54468ed6432b12eba6b1f2126ad9031fcbcae1
Aladdin Sonni <aladdin.sonni@gmail.com>
2019-11-18 13:27:57 +0200

    Errors During Development vs Production

--- middleware/error.js
+++ middleware/error.js
@@ -1,38 +1,74 @@
 const ErrorResponse = require('../utils/errorResponse');
 
-const errorHandler = (err, req, res, next) => {
-  let error = { ...err };
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new ErrorResponse(message, 400);
+};
 
-  error.message = err.message;
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new ErrorResponse(message, 400);
+};
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
 
-  // Log to console for dev
-  if (process.env.NODE_ENV === 'development') {
-    // eslint-disable-next-line no-console
-    console.log(err);
-  }
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new ErrorResponse(message, 400);
+};
 
-  // Mongoose bad ObjectId
-  if (err.name === 'CastError') {
-    const message = `Resource not found`;
-    error = new ErrorResponse(message, 404);
-  }
-
-  // Mongoose duplicate key
-  if (err.code === 11000) {
-    const message = `Duplicate field value entered`;
-    error = new ErrorResponse(message, 400);
-  }
-
-  // Mongoose validation error
-  if (err.name === 'ValidationError') {
-    const message = Object.values(err.errors).map(val => val.message);
-    error = new ErrorResponse(message, 400);
-  }
-
-  res.status(error.statusCode || 500).json({
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
     success: false,
-    error: error.message || 'Server Error'
+    error: err,
+    message: err.message,
+    stack: err.stack
   });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      success: false,
+      message: err.message
+    });
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error('ERROR 💥', err);
+    // 2) Send generic message
+    res.status(500).json({
+      success: false,
+      message: 'Something went very wrong!'
+    });
+  }
+};
+
+const errorHandler = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  err.message = err.message || 'Server Error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    if (error.name === 'CastError') {
+      error = handleCastErrorDB(error);
+    }
+
+    if (error.code === 11000) {
+      error = handleDuplicateFieldsDB(error);
+    }
+
+    if (error.name === 'ValidationError') {
+      error = handleValidationErrorDB(error);
+    }
+
+    sendErrorProd(error, res);
+  }
 };
 
 module.exports = errorHandler;
